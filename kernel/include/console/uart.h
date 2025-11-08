12dcd2f45fe52ba8c25e0d0ac732ff1addc2e298
Harry Ramsey <harryramseybusiness@gmail.com>
2025-11-08 22:11:44 +0000

    Refactor UART code from main.c
    This commit refactors code regarding UART from main.c into its own
    seperate file to enable the UART printing across the kernel.

---
+++ kernel/include/console/uart.h
@@ -0,0 +1,15 @@
+#ifndef UART_H
+#define UART_H
+
+#include <stdint.h>
+
+#define UART_BASE   0x09000000
+#define UART_CR     (UART_BASE + 0x30)
+#define UART_LCR_H  (UART_BASE + 0x2c)
+#define UART_FR     (UART_BASE + 0x18)
+
+void uart_init();
+void uart_putc(const char c);
+void uart_puts(const char *s);
+
+#endif /* UART_H */
