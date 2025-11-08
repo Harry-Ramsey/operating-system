0e3c168a8a9cccdf2094c0ee23e8d9ce0ba78660
Harry Ramsey <harryramseybusiness@gmail.com>
2025-11-08 23:15:30 +0000

    Print debug information for unhandled vector entries
    This commit enhances vector debugging by printing the exception syndrome
    register and the fault address register when an exception occurs.

--- kernel/src/vector_entries.c
+++ kernel/src/vector_entries.c
@@ -1,6 +1,20 @@
 #include "vector_entries.h"
+#include <stdint.h>
+
+#include "console/uart.h"
 
 void exception_not_implemented() {
+    uint64_t fault_address;
+    uint64_t exception_syndrome;
+    asm("mrs %[address], far_el1\n"
+        "mrs %[exception], esr_el1\n"
+        : [address] "=r" (fault_address),
+          [exception] "=r" (exception_syndrome)
+    );
+    uart_puts("FAR_EL1: ");
+    uart_puthex(fault_address);
+    uart_puts("\r\nESR_EL1: ");
+    uart_puthex(exception_syndrome);
     while(1) { }
 }
 
